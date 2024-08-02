a44ac7fde6fd1a5057f558a45b92c90e62f2e146
Willame Oliveira <willame.oliveira@ifpi.edu.br>
2024-08-02 15:47:48 -0300

    Remoção de aula 8 questao 2

---
+++ repeticao/aula9_questao02.py
@@ -0,0 +1,14 @@
+num = int(input("Digite um número inteiro positivo: "))
+soma = 0
+quant = 0
+maior = 0
+while num > 0:
+  soma += num
+  quant += 1
+  if num > maior:
+    maior = num
+  num = int(input("Digite um número inteiro positivo: "))
+print(f"A soma é: {soma}")
+media = soma / quant
+print(f"a média é: {media:.2f}")
+print(f"o maior é: {maior}")
